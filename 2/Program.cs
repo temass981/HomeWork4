cf2e31e3be76875a65ae44241866905874b373ef
Artyom <simstemasss@mail.ru>
2022-12-06 21:40:43 +0300

    second

---
+++ 2/Program.cs
@@ -0,0 +1,12 @@
+﻿System.Console.WriteLine("Введите число");
+string a =Console.ReadLine();
+int Sum(String str){
+    int sum = 0;
+    for(int i = 0; i<a.Length; i++){
+    int number = (int)Char.GetNumericValue(a[i]);
+    sum = sum + number;
+}
+    return sum;
+}
+int result = Sum(a);
+System.Console.WriteLine(result);

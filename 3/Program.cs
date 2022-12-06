7716448f5d02910081487395702b5c7fa4ac000d
Artyom <simstemasss@mail.ru>
2022-12-06 22:12:08 +0300

    Three

---
+++ 3/Program.cs
@@ -0,0 +1,16 @@
+﻿
+int [] ArrayR(int size){
+    int [] a = new int [size];
+    for(int i=0; i<a.Length; i++){
+        a[i]=new Random().Next(0,10);
+    }
+    return a;
+}
+
+void Out(int [] array){
+    for (int i=0; i<array.Length; i++){
+        System.Console.WriteLine(array[i]);
+    }
+}
+int [] result = ArrayR(8);
+Out(result);

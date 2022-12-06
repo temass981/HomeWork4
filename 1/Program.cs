27f2b5f468ec00250b3b277d4866133d3d490a0f
Artyom <simstemasss@mail.ru>
2022-12-06 21:14:55 +0300

    first

---
+++ 1/Program.cs
@@ -0,0 +1,10 @@
+﻿System.Console.WriteLine("Введите число");
+int a = int.Parse(Console.ReadLine());
+System.Console.WriteLine("Введите число");
+int b = int.Parse(Console.ReadLine());
+int  Degree(int x, int b){
+    int result = (int)Math.Pow(a,b);
+    return result;
+}
+int result = Degree(a,b);
+System.Console.WriteLine(result);
